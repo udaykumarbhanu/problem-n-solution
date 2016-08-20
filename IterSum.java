c71f3df39d62d64fb29f2ee97f6276561394f2e4
Uday Kumar Bhanu <udaykumarbhanu@gmail.com>
2016-08-20 18:26:18 +0530

    Iterative way to get sum 0 to num

---
+++ IterSum.java
@@ -0,0 +1,13 @@
+
+public class IterSum {
+
+	public static void main(String[] args) {
+		int num = 4;
+		int res = 0;
+		for (int i = 0; i <= num; i++) {
+			res = res + i;
+		}
+		System.out.println("Result is " + res);
+	}
+
+}
