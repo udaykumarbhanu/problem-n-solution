46fb45179905fbf31a60310655002330d6e7cde7
Uday Kumar Bhanu <udaykumarbhanu@gmail.com>
2018-04-08 18:36:52 +0530

    palindrome addded 3rd method

--- palindromes.py
+++ palindromes.py
@@ -19,9 +19,32 @@ def palindrome_two(s):
 	return s == s[::-1]
 
 
+def palindrome_three(s):
+	"""
+	:type s: string
+	:rtype: boolean
+	"""
+	if s:
+		start_index = 0
+		end_index = len(s) - 1
+
+		while start_index < end_index:
+			if s[start_index] != s[end_index]:
+
+				return False
+			else:
+				start_index += 1
+				end_index -= 1
+
+		return True
+
+
 if __name__ == "__main__":
 	s1="aba"
 	print palindrome_one(s1)
 	
-	s2="chasmish"
+	s2="donna"
 	print palindrome_two(s2)
+
+	s3="uday"
+	print palindrome_three(s3)
